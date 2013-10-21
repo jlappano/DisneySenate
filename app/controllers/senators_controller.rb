67870834963337a441c2c21245f879460ea1dc3c
Julie lappano <julie.lappano@gmail.com>
2013-10-21 17:18:54 -0400

    zipcode form

--- app/controllers/senators_controller.rb
+++ app/controllers/senators_controller.rb
@@ -1,3 +1,16 @@
 class SenatorsController < ApplicationController
 
+  def new
+    # @senator = Senator.new
+    render :new
+  end
+
+  def create
+    @district = Congress.legistators_locate(params[:zipcode])
+    district_sen = @district["results"].select {|leg| leg["title"] == "Sen" && leg["state_rank"] == "senior"}
+  end
+
+  def show
+  end
+
 end
